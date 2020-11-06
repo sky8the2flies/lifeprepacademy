2a09f45c3c3cdf0b3f5017f30378a9a9bf6ce9c0
Eric Moore <eric@educate-ca.org>
2020-11-05 18:48:23 -0800

    Sign Up

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     # GENERAL
     path('', views.HomePageView.as_view(), name="home"),
+    
+    # AUTH
+    path('accounts/signup/', views.signup, name="signup"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 class HomePageView (TemplateView):
     template_name = "main_app/home.html"
+
+def signup (request):
+    if request.method == "POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request, user)
+            return redirect ("home")
+        
+    form=UserCreationForm()
+    return render(request, 'registration/signup.html', {'form':form})
